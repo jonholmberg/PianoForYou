ef525cc7c07568d2d7972248220cce6d1b5c514a
Jon Holmberg <jon.holmberg@visma.com>
2018-06-11 18:20:15 +0200

    Initial Commit

---
+++ PianoForYouTests/PianoForYouTests.swift
@@ -0,0 +1,36 @@
+//
+//  PianoForYouTests.swift
+//  PianoForYouTests
+//
+//  Created by Jon Holmberg on 11/06/2018.
+//  Copyright © 2018 Bent. All rights reserved.
+//
+
+import XCTest
+@testable import PianoForYou
+
+class PianoForYouTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
